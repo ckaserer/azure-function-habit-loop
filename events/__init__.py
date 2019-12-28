76ee2185207cc938bdd71f7aae0533cceedf6ee0
Clemens Kaserer <clemens.kaserer@gmail.com>
2019-12-29 00:03:00 +0100

    put functionality

--- events/__init__.py
+++ events/__init__.py
@@ -1,42 +1,53 @@
-import sys
 import os
+import sys
 from flask import Flask
-from flask_restful import reqparse, Api, Resource
-import json
+from flask_restful import reqparse, abort, Api, Resource
 import pyodbc
-
-# This is a simplified example that only support GET request.
-# It is meant to help you to get you started if you're new to development
-# and to show how simple is using Azure SQL with Python
-# A more complete example is in "app.py"
-# To run this simplified sample follow the README, but instead of running "flask run"
-# just run "python ./simple-app.py"
-# Enjoy!
+import time
 
 # Initialize Flask
 app = Flask(__name__)
 
 # Setup Flask Restful framework
 api = Api(app)
-parser = reqparse.RequestParser()
-parser.add_argument('event_name')
 
 # Create connection to Azure SQL
 conn = pyodbc.connect(os.environ['SQLAZURECONNSTR_WWIF'])
 
-class Events(Resource):
-    def get(self, event_name):   
-        cursor = conn.cursor()    
-        cursor.execute(f"SELECT ID,habit,occured FROM events WHERE CONVERT(VARCHAR, habit) = '{event_name}' ORDER BY occured DESC")
-        columns = [column[0] for column in cursor.description]
-        result = []
-        for row in cursor.fetchall():
-            result.append(dict(zip(columns, row)))
-        cursor.close()
+class Queryable(Resource):
+    def executeQueryJson(self, myquery):
+        result = {}        
+        cursor = conn.cursor()  
+        try:
+            cursor.execute(f"{myquery}")
+            columns = [column[0] for column in cursor.description]
+            result = []
+            for row in cursor.fetchall():
+                result.append(dict(zip(columns, row)))
+            cursor.commit()    
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
+        finally:    
+            cursor.close()
+        return result
+
+# Habit Class
+class Habit(Queryable):
+    def get(self, habit):   
+        result = self.executeQueryJson(f"SELECT ID,habit,occured FROM events WHERE CONVERT(VARCHAR, habit) = '{habit}' ORDER BY occured DESC")   
+        return result, 200
+
+    def post(self, habit):   
+        result = self.executeQueryJson(f"SELECT ID,habit,occured FROM events WHERE CONVERT(VARCHAR, habit) = '{habit}' ORDER BY occured DESC")   
         return result, 200
     
-# Create API route to defined Customer class
-api.add_resource(Events, '/events/get/', '/events/get/<event_name>')
+    def put(self, habit):
+        result = self.executeQueryJson(f"INSERT INTO events(habit,occured) OUTPUT INSERTED.id,INSERTED.habit,INSERTED.occured VALUES ('{habit}', '{int(time.time())}')")
+        return result, 201
+    
+# # Create API routes
+api.add_resource(Habit, '/habit', '/habit/<habit>')
 
 # Start App
 if __name__ == '__main__':
